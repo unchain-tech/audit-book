5da6f1e2223dc6429eb72c5f97b72b535ad9293c
SHØ <neila@users.noreply.github.com>
2023-02-22 04:26:36 +0900

    typo fix

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -4,7 +4,7 @@ const buildLintCommand = (filenames) =>
     `eslint ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`;
 
 const buildSolhintCommand = (filenames) =>
-    `solhint --max-error 0 -c packages/contract/.solhint.json ${filenames
+    `solhint --max-warning 0 -c packages/contract/.solhint.json ${filenames
         .map((f) => path.relative(process.cwd(), f))
         .join(' ')} `;
 
