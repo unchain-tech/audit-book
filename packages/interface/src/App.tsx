a1bd7a6f9e5aa5a74eeba4c1622fa69d6d2335f6
yosuke <yosuke2002jp@yahoo.co.jp>
2023-03-06 19:51:49 +0900

    プリティアで整えた！

--- packages/interface/src/App.tsx
+++ packages/interface/src/App.tsx
@@ -4,128 +4,134 @@ import frontcover from './frontcover.png';
 import './App.css';
 import { ethers } from 'ethers';
 
-{/************************************ここからグローバルな定数************************************/}
+{
+    /************************************ここからグローバルな定数************************************/
+}
 //const abi = contract.abi;
-const contractAddress = "0xbDBebF9b9f41C6BCAf9CbC26290Ddc07ea0F490B";
-const MaticTestnetMumbaiNetworkChainId = "0x13881";
-
+const contractAddress = '0xbDBebF9b9f41C6BCAf9CbC26290Ddc07ea0F490B';
+const MaticTestnetMumbaiNetworkChainId = '0x13881';
 
 function App() {
-
     const [currentAccount, setCurrentAccount] = useState<string | null>();
-    const [metamaskError, setMetamaskError]  = useState<boolean | null>();
+    const [metamaskError, setMetamaskError] = useState<boolean | null>();
     const [mineStatus, setMineStatus] = useState<string | null>();
-    const [totalMintCount, setTotalMintCount]  = useState<number | null>();
+    const [totalMintCount, setTotalMintCount] = useState<number | null>();
     const [showToast, setShowToast] = useState(false);
     const [iaLoading, setIsLoading] = useState(false);
 
-    const connectWallet = async () => { 
-        const { ethereum } = window as any;    // Buttonクリックで実行 -> クライアントサイドの処理なので、windowが参照できethereumが扱える
+    const connectWallet = async () => {
+        const { ethereum } = window as any; // Buttonクリックで実行 -> クライアントサイドの処理なので、windowが参照できethereumが扱える
         // ウォレット接続処理
         if (!ethereum) {
-            alert("Please install Metamask!");
+            alert('Please install Metamask!');
         }
-    
+
         if (ethereum.networkVersion !== MaticTestnetMumbaiNetworkChainId) {
-          try {
-            // Mumbai testnet に切り替えます。
-            await ethereum.request({
-              method: 'wallet_switchEthereumChain',
-              params: [{ chainId: '0x13881' }], // utilsフォルダ内のnetworks.js を確認しましょう。0xは16進数です。
-            });
-          } catch (error: any) {
-            // このエラーコードは当該チェーンがメタマスクに追加されていない場合です。
-            // その場合、ユーザーに追加するよう促します。
-            if (error.code === 4902) {
-              try {
+            try {
+                // Mumbai testnet に切り替えます。
                 await ethereum.request({
-                  method: 'wallet_addEthereumChain',
-                  params: [
-                    {
-                      chainId: '0x13881',
-                      chainName: 'Polygon Mumbai Testnet',
-                      rpcUrls: ['https://rpc-mumbai.maticvigil.com/'],
-                      nativeCurrency: {
-                          name: "Mumbai Matic",
-                          symbol: "MATIC",
-                          decimals: 18
-                      },
-                      blockExplorerUrls: ["https://mumbai.polygonscan.com/"]
-                    },
-                  ],
+                    method: 'wallet_switchEthereumChain',
+                    params: [{ chainId: '0x13881' }], // utilsフォルダ内のnetworks.js を確認しましょう。0xは16進数です。
                 });
-              } catch (error) {
+            } catch (error: any) {
+                // このエラーコードは当該チェーンがメタマスクに追加されていない場合です。
+                // その場合、ユーザーに追加するよう促します。
+                if (error.code === 4902) {
+                    try {
+                        await ethereum.request({
+                            method: 'wallet_addEthereumChain',
+                            params: [
+                                {
+                                    chainId: '0x13881',
+                                    chainName: 'Polygon Mumbai Testnet',
+                                    rpcUrls: [
+                                        'https://rpc-mumbai.maticvigil.com/',
+                                    ],
+                                    nativeCurrency: {
+                                        name: 'Mumbai Matic',
+                                        symbol: 'MATIC',
+                                        decimals: 18,
+                                    },
+                                    blockExplorerUrls: [
+                                        'https://mumbai.polygonscan.com/',
+                                    ],
+                                },
+                            ],
+                        });
+                    } catch (error) {
+                        console.log(error);
+                    }
+                }
                 console.log(error);
-              }
             }
-            console.log(error);
-          }
         }
-    
-        
-    
+
         try {
             const network = await ethereum.request({ method: 'eth_chainId' });
-      
+
             if (network.toString() === '0x13881') {
-              const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-              console.log("Found an account! Address: ", accounts[0]);
-              setMetamaskError(null);
-              setCurrentAccount(accounts[0]);
+                const accounts = await ethereum.request({
+                    method: 'eth_requestAccounts',
+                });
+                console.log('Found an account! Address: ', accounts[0]);
+                setMetamaskError(null);
+                setCurrentAccount(accounts[0]);
+            } else {
+                setMetamaskError(true);
             }
-      
-            else {
-              setMetamaskError(true);
-            }
-      
-          } catch (err) {
-            console.log(err)
-          }
-      }
-
-      const mask = function (number:any) {
-        const cha = String(number);
-        const topvisible = number.slice(0,4);
-        const bottomvisible = number.slice(-4);
-        return(
-          <p color="white">{topvisible}...{bottomvisible}</p>
-        )
-      };
-    
-      const mintNFT = async () => {
-      }
-
-      const renderButtun = ( bname:any, isOnClick:any, ahref:any ) => {
-        if( isOnClick ){
-          return  <button
-                    className="button-base connect-wallet-button2" 
-                    onClick={connectWallet}>
-                  { bname }
-                </button>;
-        }else{
-          return(
-                <a>
-                    <button
-                        className="button-base connect-wallet-button2"
-                        >
-                    { bname }
-                    </button>
-                </a>);
+        } catch (err) {
+            console.log(err);
         }
-      };
-      const renderMintButtun = () => {
-        return  <button
-                    className="button-base connect-wallet-button2"
-                    onClick={mintNFT}
-                >
-                    教科書をミント
-                </button>;
-      };
+    };
 
-      const renderDisplayButton = () => {
-        return(
-          <div>
-            {/*{chain && <div>Connected to {chain.name}</div>}
+    const mask = function (number: any) {
+        const cha = String(number);
+        const topvisible = number.slice(0, 4);
+        const bottomvisible = number.slice(-4);
+        return (
+            <p color="white">
+                {topvisible}...{bottomvisible}
+            </p>
+        );
+    };
+
+    const mintNFT = async () => {};
+
+    const renderButtun = (bname: any, isOnClick: any, ahref: any) => {
+        if (isOnClick) {
+            return (
+                <button
+                    className="button-base connect-wallet-button2"
+                    onClick={connectWallet}
+                >
+                    {bname}
+                </button>
+            );
+        } else {
+            return (
+                <a>
+                    <button className="button-base connect-wallet-button2">
+                        {bname}
+                    </button>
+                </a>
+            );
+        }
+    };
+    const renderMintButtun = () => {
+        return (
+            <button
+                className="button-base connect-wallet-button2"
+                onClick={mintNFT}
+            >
+                教科書をミント
+            </button>
+        );
+    };
+
+    const renderDisplayButton = () => {
+        return (
+            <div>
+                {/*{chain && <div>Connected to {chain.name}</div>}
             <button
               disabled={!switchNetwork || chains[0].id === chain?.id}
               key={chains[0].id}
@@ -135,31 +141,35 @@ function App() {
               {isLoading && pendingChainId === chains[0].id && ' (switching)'}
             </button>
             <div>{error && error.message}</div>*/}
-            {!currentAccount && renderButtun("ウォレットを接続",true,"")}
-            {currentAccount && !totalMintCount && !iaLoading &&
-              <div>
-                {renderMintButtun()}
-              </div>}
-            {currentAccount && totalMintCount && !iaLoading &&
-            <div>
-              {renderButtun("NFT at OpenSea",false,`https://testnets.opensea.io/ja/assets/mumbai/${contractAddress}/${totalMintCount}`)}
-              <p>Congrats! Your NFT minted! </p>
-            </div>}
-            {currentAccount && !totalMintCount && iaLoading &&
-            <div>
-              {/*<Spinner
+                {!currentAccount && renderButtun('ウォレットを接続', true, '')}
+                {currentAccount && !totalMintCount && !iaLoading && (
+                    <div>{renderMintButtun()}</div>
+                )}
+                {currentAccount && totalMintCount && !iaLoading && (
+                    <div>
+                        {renderButtun(
+                            'NFT at OpenSea',
+                            false,
+                            `https://testnets.opensea.io/ja/assets/mumbai/${contractAddress}/${totalMintCount}`
+                        )}
+                        <p>Congrats! Your NFT minted! </p>
+                    </div>
+                )}
+                {currentAccount && !totalMintCount && iaLoading && (
+                    <div>
+                        {/*<Spinner
                 thickness='4px'
                 speed='0.65s'
                 emptyColor='gray.200'
                 color='#f6a429'
                 size='xl'
                 />*/}
-              <p>Please wait just a little bit more.</p>
+                        <p>Please wait just a little bit more.</p>
+                    </div>
+                )}
             </div>
-            }
-          </div>
-        )
-      }
+        );
+    };
 
     return (
         <div className="App">
@@ -210,11 +220,9 @@ function App() {
                 {/*<button className="button-base connect-wallet-button2" onClick={connectWallet}>
                     ウォレット接続
                 </button>*/}
-                {currentAccount && !totalMintCount && !iaLoading &&
-                    <div>
-                        ウォレット接続完了！
-                    </div>
-                }
+                {currentAccount && !totalMintCount && !iaLoading && (
+                    <div>ウォレット接続完了！</div>
+                )}
             </div>
             <footer className="footer">
                 <p className="copyright">
