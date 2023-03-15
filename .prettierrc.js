7d98341e7f8c3927bfdb770de15fe88438fffdea
SHØ <neila@users.noreply.github.com>
2023-03-15 20:45:36 +0900

    prettier sort imports

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,4 +3,10 @@ module.exports = {
     tabWidth: 4,
     trailingComma: 'es5',
     singleQuote: true,
+    importOrder: ['^[./]', '^@/(.*)$'],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+    importOrderGroupNamespaceSpecifiers: true,
+    importOrderCaseInsensitive: true,
+    plugins: ['@trivago/prettier-plugin-sort-imports'],
 };
