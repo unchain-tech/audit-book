7abe1c4c417254241b3213310d05c2f3bfc162c2
SHØ <neila@users.noreply.github.com>
2023-03-28 16:26:37 +0900

    apploy format

--- packages/contracts/scripts/argument.ts
+++ packages/contracts/scripts/argument.ts
@@ -1,6 +1,7 @@
 import { ethers } from 'hardhat';
+
 module.exports = [
-    '0x4491d1c47bbde6746f878400090ba6935a91dab6',
-    ethers.utils.parseEther('1000'),
-    '',
+  '0x4491d1c47bbde6746f878400090ba6935a91dab6',
+  ethers.utils.parseEther('1000'),
+  '',
 ];
